c508c162cba15fdbd740eb9c2e0722819284f27c
Andres Court <andres@andrescourt.com>
2023-09-15 00:17:07 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module test/gorilla-middleware
+
+go 1.21.1
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	gitlab.com/0x4149/logz v0.0.0-20221128142445-4d4999b51cce // indirect
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"gitlab.com/0x4149/logz"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello World")
+}
+func AuthRoutes(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Auth Routes")
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		logz.Info(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+func init() {
+	logz.VerbosMode()
+
+	logz.Run()
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+
+	auth := r.PathPrefix("/auth").Subrouter()
+	auth.HandleFunc("", AuthRoutes)
+	auth.Use(loggingMiddleware)
+
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "42069"
+	}
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf(":%s", port),
+		WriteTimeout: time.Second * 10,
+		ReadTimeout:  time.Second * 10,
+	}
+
+	logz.Info("Server running on port ", port)
+	logz.Fatal(srv.ListenAndServe())
+}
